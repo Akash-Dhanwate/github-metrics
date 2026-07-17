259839b3ee68b4bd20d6cfb49acf18cb34d81894
Akash Dhanwate <akashdhanawate117@gmail.com>
2026-07-17 07:06:15 +0530

    added the files

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000"
+        "https://github-metrics-1.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
